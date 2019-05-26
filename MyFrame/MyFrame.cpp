ed5fff35491cd124739e568fcb5c0088d1df7e4a
lkp <781171312@qq.com>
2019-05-26 21:38:05 +0800

    添加my_handle函数

--- MyFrame/MyFrame.cpp
+++ MyFrame/MyFrame.cpp
@@ -2,7 +2,7 @@
 #include "MyApp.h"
 #include "MyContext.h"
 
-extern "C" int my_send(MyContext* ctx,
+int my_send(MyContext* ctx,
             uint32_t source,
             uint32_t destination ,
             int type, /* 消息类型 */
@@ -13,8 +13,13 @@ extern "C" int my_send(MyContext* ctx,
     return ctx->SendMsg(source, destination, type, session, msg, sz);
 }
 
-extern "C" void my_callback(MyContext* ctx, my_cb cb, void* ud)
+void my_callback(MyContext* ctx, my_cb cb, void* ud)
 {
     ctx->SetCB(cb, ud);
+}
+
+uint32_t my_handle(MyContext* ctx)
+{
+    return ctx->GetHandle();
 }
 
--- MyFrame/MyFrame.h
+++ MyFrame/MyFrame.h
@@ -26,6 +26,8 @@ int my_send(MyContext* ctx,
          void * msg,/* 消息内容 */
          size_t sz /* 消息长度 */);
 
+uint32_t my_handle(MyContext* ctx);
+
 void my_callback(MyContext* ctx, my_cb cb, void* ud);
 
 
