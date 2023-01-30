63d6de7c8ed54afea19b4dfcb5dbe6fbf00b6f3d
likepeng01 <likepeng01@baidu.com>
2023-01-30 17:31:50 +0800

    add worker quit example

---
+++ examples/example_worker_quit.cpp
@@ -0,0 +1,31 @@
+/****************************************************************************
+Copyright (c) 2018, likepeng
+All rights reserved.
+
+Author: likepeng <likepeng0418@163.com>
+****************************************************************************/
+#include <chrono>
+#include <thread>
+
+#include <glog/logging.h>
+
+#include "myframe/actor.h"
+#include "myframe/msg.h"
+#include "myframe/worker.h"
+
+class ExampleWorkerQuit : public myframe::Worker {
+ public:
+  ExampleWorkerQuit() {}
+  virtual ~ExampleWorkerQuit() {}
+
+  void Run() override {
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    Stop();
+  }
+};
+
+/* 创建worker实例函数 */
+extern "C" std::shared_ptr<myframe::Worker> my_worker_create(
+    const std::string& worker_name) {
+  return std::make_shared<ExampleWorkerQuit>();
+}
