1f20e9c8d743f197b6de00cc4c413bb46ee60b04
likepeng <likepeng0418@163.com>
2023-05-04 15:50:44 +0800

    launcher add sys_conf option

--- launcher/module_argument.h
+++ launcher/module_argument.h
@@ -18,7 +18,7 @@ class ModuleArgument final {
     kInvalidArgument,
     kOtherParameter,
   };
-  ModuleArgument() = default;
+  ModuleArgument(const std::string& sys_conf_dir);
   ~ModuleArgument() = default;
 
   OptionReturnType ParseArgument(const int argc, char* const argv[]);
@@ -28,11 +28,20 @@ class ModuleArgument final {
   inline std::string GetBinaryName() const { return binary_name_; }
   inline std::string GetProcessName() const { return process_name_; }
   inline std::string GetCmd() const { return cmd_; }
+  inline int GetThreadPoolSize() const { return thread_poll_size_; }
+  inline int GetConnEventSize() const { return conn_event_size_; }
+  inline int GetWarningMsgSize() const { return warning_msg_size_; }
  private:
+  bool ParseSysConf(const std::string&);
+
+  int thread_poll_size_{4};
+  int conn_event_size_{2};
+  int warning_msg_size_{10};
   std::string cmd_{""};
   std::string binary_name_{""};
   std::string process_name_{""};
   std::string conf_dir_{""};
+  std::string sys_conf_dir_{"conf"};
   std::list<std::string> conf_list_;
 };
 
