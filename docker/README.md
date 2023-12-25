122da6901e2a29d0748c6ac423f8f7532af01141
likepeng <likepeng0418@163.com>
2023-12-25 16:42:36 +0800

    support docker

---
+++ docker/README.md
@@ -0,0 +1,29 @@
+# myframe docker
+主要用于创建myframe docker容器镜像；
+包含编译好的myframe，可以运行example，benchmark和创建运行组件工程。
+
+## 目录
+myframe安装目录：`/usr/local`
+
+## 构建docker镜像
+```sh
+bash docker_build.bash <myframe_version>
+```
+
+## 创建运行docker容器
+```sh
+docker run -itd \
+    --name "myframe_run" \
+    myframe:${<myframe_version>} \
+    /bin/bash
+```
+
+## 进入docker容器
+```sh
+docker exec -it \
+    -it "myframe_run" \
+    /bin/bash
+```
+
+## 在docker中开发
+[开发手册](../doc/development_guide.md)
---
+++ docker/docker_build.bash
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+readonly PWD0="$(dirname "$(readlink -fn "$0")")"
+readonly MYFRAME_ROOT="$(dirname ${PWD0})"
+
+readonly IMAGE_NAME="myframe"
+
+set -e
+
+function main {
+  cd "${PWD0}"
+
+  local -a cmd=()
+  if command -v docker; then
+    cmd+=("docker" "build")
+  else
+    echo "No docker existed!"
+    exit 1
+  fi
+
+  local arc="$(uname -m)"
+  case "${arc}" in
+  "aarch64" | "arm64")
+    arc="aarch64"
+    ;;
+  "x86_64" | "amd64")
+    arc="amd64"
+    ;;
+  *)
+    echo "Unknown arch: ${arc}"
+    exit 1
+    ;;
+  esac
+
+  local tag="${arc}-$1"
+
+  "${cmd[@]}" \
+    --build-arg myframe_version=$1 \
+    --ulimit nofile=102400:102400 \
+    -f "ubuntu2004.dockerfile" \
+    -t "${IMAGE_NAME}:${tag}" \
+    "${MYFRAME_ROOT}"
+}
+
+if [ "$#" -ne 1 ]; then
+    echo "Error: No arguments provided. Usage: $0 \"myframe_version\""
+    exit 1
+fi
+
+main "$@"
